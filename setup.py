3ff41ad0e199dbbfd57b20a0b6825b8ce7805793
hardboiled65 <hardboiled65@gmail.com>
2019-01-11 00:02:49 +0900

    Add scripts.

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
     name='slm',
     packages=[],
-    version='0.0.1',
+    scripts=['slm'],
+    version='0.0.2',
     license='MIT',
     description='Seshat library manager.',
     author='hardboiled65',
     author_email='hardboiled65@gmail.com',
     url='https://github.com/hardboiled65/slm',
-    download_url='https://github.com/hardboiled65/slm/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/hardboiled65/slm/archive/v0.0.2.tar.gz',
     keywords=['c', 'c++'],
     install_requires=[
         'pyyaml',
