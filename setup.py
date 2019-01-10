c3251fe2c9d96312d09d378419e3ebb0e469a40e
hardboiled65 <hardboiled65@gmail.com>
2019-01-10 23:55:37 +0900

    Prepare to distribute.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='slm',
+    packages=[],
+    version='0.0.1',
+    license='MIT',
+    description='Seshat library manager.',
+    author='hardboiled65',
+    author_email='hardboiled65@gmail.com',
+    url='https://github.com/hardboiled65/slm',
+    download_url='https://github.com/hardboiled65/slm/archive/v0.0.1.tar.gz',
+    keywords=['c', 'c++'],
+    install_requires=[
+        'pyyaml',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
